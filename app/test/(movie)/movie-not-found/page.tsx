1543b5f2523b9b05f14c1187eeb22f870d5bab94
mini <jma1020@naver.com>
2023-05-23 00:25:38 +0900

    not found 라우팅 확인

---
+++ app/test/(movie)/movie-not-found/page.tsx
@@ -0,0 +1,20 @@
+import { notFound } from "next/navigation";
+
+const getMovie = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1545345");
+  if (!res.ok) {
+    console.log("not found");
+    notFound();
+  }
+
+  return res.json();
+};
+
+export default async function NotFoundMovie() {
+  const movie = await getMovie();
+  return (
+    <div>
+      <h1>user data</h1>
+    </div>
+  );
+}
