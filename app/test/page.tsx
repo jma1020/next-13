294827d9ca8096075dff46c072818c022c9fe206
mini <jma1020@naver.com>
2023-06-05 15:21:54 +0900

    tailwind  Arbitrary variants 적용 테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,9 +3,41 @@
 import styled from "styled-components";
 
 export default function Test() {
-  return <Mc>테스트 페이지</Mc>;
+  return (
+    <Mc>
+      테스트
+      <div className=" [&>*:nth-child(3)_p:first-child]:text-teal-500 ">
+        {/* 3번쨰 자식 중 자손이 p로 시작하는 친구들  */}
+        sad
+        <div>
+          asd
+          <div>색</div>
+          <div>색</div>
+          <p>색</p>
+        </div>
+        <div>
+          asd
+          <div>색</div>
+        </div>
+        <div>
+          as
+          <div>
+            색<p>이것도</p>
+          </div>
+          <p>test</p>
+          <p>이test것도</p>
+          <p>
+            test
+            <p>이것도</p>
+          </p>
+          <p>test</p>
+        </div>
+      </div>
+      페이지
+    </Mc>
+  );
 }
 
 const Mc = styled.div`
-  background-color: red;
+  background-color: white;
 `;
